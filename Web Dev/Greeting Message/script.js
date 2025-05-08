f2730fbf02e2d0651aed19ccbb19cd2b48a251a4
Dhwaj Jain <dhwaj054@gmail.com>
2025-05-08 18:34:56 +0530

    Create script.js

---
+++ Web Dev/Greeting Message/script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    // Option 1: Using alert
+    // alert('Hello, Student!');
+
+    // Option 2: Updating the DOM
+    document.getElementById('message').textContent = 'Hello, Student!';
+}
